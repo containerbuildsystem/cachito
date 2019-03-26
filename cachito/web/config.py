94bfb57b6fa7d0e746635d6390b5b727970f50b0
mprahl <mprahl@users.noreply.github.com>
2019-03-26 15:49:07 -0400

    Add the ability to configure the Cachito Flask app

---
+++ cachito/web/config.py
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+class Config(object):
+    """The base Cachito Flask configuration."""
+    pass
+
+
+class ProductionConfig(Config):
+    """The production Cachito Flask configuration."""
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    """The development Cachito Flask configuration."""
+    pass
+
+
+class TestingConfig(DevelopmentConfig):
+    """The testing Cachito Flask configuration."""
+    pass
