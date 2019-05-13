1a05aa5ebf155749e27abb714de52dac56beb67a
mprahl <mprahl@users.noreply.github.com>
2019-05-13 14:08:00 -0400

    Don't allow the columns in the request_pkg_manager association table to be null

--- cachito/web/models.py
+++ cachito/web/models.py
@@ -7,8 +7,8 @@ from cachito.web import db
 
 request_pkg_manager_table = db.Table(
     'request_pkg_manager',
-    db.Column('request_id', db.Integer, db.ForeignKey('request.id')),
-    db.Column('pkg_manager_id', db.Integer, db.ForeignKey('package_manager.id'))
+    db.Column('request_id', db.Integer, db.ForeignKey('request.id'), nullable=False),
+    db.Column('pkg_manager_id', db.Integer, db.ForeignKey('package_manager.id'), nullable=False)
 )
 
 
