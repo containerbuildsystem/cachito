2d41915575855ff5ac7cf01200729069f9487765
mprahl <mprahl@users.noreply.github.com>
2020-02-11 16:42:19 -0500

    Add the state query parameter in the pagination when used

--- cachito/web/utils.py
+++ cachito/web/utils.py
@@ -2,22 +2,22 @@
 from flask import request, url_for
 
 
-def pagination_metadata(pagination_query, verbose):
+def pagination_metadata(pagination_query, **kwargs):
     """
     Return a dictionary containing metadata about the paginated query.
     This must be run as part of a Flask request.
 
     :param pagination_query: flask_sqlalchemy.Pagination object
+    :param dict kwargs: the query parameters to add to the URLs
     :return: a dictionary containing metadata about the paginated query
     """
-
     pagination_data = {
         'first': url_for(
             request.endpoint, page=1, per_page=pagination_query.per_page,
-            verbose=verbose, _external=True),
+            _external=True, **kwargs),
         'last': url_for(
             request.endpoint, page=pagination_query.pages, per_page=pagination_query.per_page,
-            verbose=verbose, _external=True),
+            _external=True, **kwargs),
         'next': None,
         'page': pagination_query.page,
         'pages': pagination_query.pages,
@@ -29,11 +29,11 @@ def pagination_metadata(pagination_query, verbose):
     if pagination_query.has_prev:
         pagination_data['previous'] = url_for(
             request.endpoint, page=pagination_query.prev_num,
-            per_page=pagination_query.per_page, verbose=verbose, _external=True)
+            per_page=pagination_query.per_page, _external=True, **kwargs)
     if pagination_query.has_next:
         pagination_data['next'] = url_for(
             request.endpoint, page=pagination_query.next_num,
-            per_page=pagination_query.per_page, verbose=verbose, _external=True)
+            per_page=pagination_query.per_page, _external=True, **kwargs)
 
     return pagination_data
 
