5e1ab780b366ae1ba118acf6b372603aa2720100
Adam Cmiel <acmiel@redhat.com>
2020-08-05 10:23:44 +0200

    Implement setup.py handling for Pip
    * CLOUDBLD-2053
    
    Supports only basic use cases, see docstrings in SetupPY class for more
    details.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/test_workers/test_pkg_managers/test_pip.py
+++ tests/test_workers/test_pkg_managers/test_pip.py
@@ -302,7 +302,7 @@ class TestSetupCFG:
                     "Resolving metadata.version in setup.cfg from 'attr: missing_attr.__ver__'",
                     "Attempting to find attribute '__ver__' in 'missing_attr'",
                     "Found module 'missing_attr' at '{tmpdir}/missing_attr.py'",
-                    "Could not find attribute in 'missing_attr': Attribute '__ver__' not found",
+                    "Could not find attribute in 'missing_attr': '__ver__' not found",
                 ],
                 None,
             ),
@@ -323,7 +323,7 @@ class TestSetupCFG:
                     "Found module 'not_a_literal' at '{tmpdir}/not_a_literal.py'",
                     (
                         "Could not find attribute in 'not_a_literal': "
-                        "Attribute '__ver__' is not assigned to a literal expression"
+                        "'__ver__' is not assigned to a literal expression"
                     ),
                 ],
                 None,
@@ -654,3 +654,466 @@ class TestSetupCFG:
         self._test_version_with_file_tree(
             project_tree, expect_version, expect_logs, expect_error, tmpdir, caplog
         )
+
+
+class TestSetupPY:
+    """SetupPY tests."""
+
+    @pytest.mark.parametrize("exists", [True, False])
+    def test_exists(self, exists, tmpdir):
+        """Test file existence check."""
+        if exists:
+            tmpdir.join("setup.py").write("")
+
+        setup_py = pip.SetupPY(tmpdir.strpath)
+        assert setup_py.exists() == exists
+
+    def _test_get_value(self, tmpdir, caplog, script_content, expect_val, expect_logs, what="name"):
+        """Test getting name or version from setup.py."""
+        tmpdir.join("setup.py").write(script_content.format(what=what))
+        setup_py = pip.SetupPY(tmpdir.strpath)
+
+        if what == "name":
+            value = setup_py.get_name()
+        elif what == "version":
+            value = setup_py.get_version()
+        else:
+            assert False, "'what' must be one of 'name', 'version'"
+
+        assert value == expect_val
+
+        logs = expect_logs.copy()
+        # Does not actually have to be at index 0, this is just to be more obvious
+        logs.insert(0, f"Parsing setup.py at '{tmpdir.join('setup.py')}'")
+        if expect_val is None:
+            msg = (
+                "Version in setup.py was either not found, or failed to resolve to a valid value"
+                if what == "version"
+                else "Name in setup.py was either not found, or failed to resolve to a valid string"
+            )
+            logs.append(msg)
+        else:
+            logs.append(f"Found {what} in setup.py: '{expect_val}'")
+
+        for log in logs:
+            assert log.format(tmpdir=tmpdir.strpath, what=what) in caplog.text
+
+    @pytest.mark.parametrize(
+        "script_content, expect_val, expect_logs",
+        [
+            ("", None, ["File does not seem to have a setup call"]),
+            ("my_module.setup()", None, ["File does not seem to have a setup call"]),
+            (
+                "syntax error",
+                None,
+                ["Syntax error when parsing setup.py: invalid syntax (setup.py, line 1)"],
+            ),
+            (
+                # Note that it absolutely does not matter whether you imported anything
+                "setup()",
+                None,
+                [
+                    "Found setup call on line 1",
+                    "Pseudo-path: Module.body[0] -> Expr(#1).value",
+                    "setup kwarg '{what}' not found",
+                ],
+            ),
+            (
+                "setuptools.setup()",
+                None,
+                [
+                    "Found setup call on line 1",
+                    "Pseudo-path: Module.body[0] -> Expr(#1).value",
+                    "setup kwarg '{what}' not found",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    from setuptools import setup; setup()
+                    """
+                ),
+                None,
+                [
+                    "Found setup call on line 1",
+                    "Pseudo-path: Module.body[1] -> Expr(#1).value",
+                    "setup kwarg '{what}' not found",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    from setuptools import setup
+
+                    setup()
+                    """
+                ),
+                None,
+                [
+                    "Found setup call on line 3",
+                    "Pseudo-path: Module.body[1] -> Expr(#3).value",
+                    "setup kwarg '{what}' not found",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    from setuptools import setup
+
+                    setup({what}=None)
+                    """
+                ),
+                None,
+                [
+                    "Found setup call on line 3",
+                    "Pseudo-path: Module.body[1] -> Expr(#3).value",
+                    "setup kwarg '{what}' is a literal: None",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    from setuptools import setup
+
+                    setup({what}="foo")
+                    """
+                ),
+                "foo",
+                [
+                    "Found setup call on line 3",
+                    "Pseudo-path: Module.body[1] -> Expr(#3).value",
+                    "setup kwarg '{what}' is a literal: 'foo'",
+                ],
+            ),
+        ],
+    )
+    @pytest.mark.parametrize("what", ["name", "version"])
+    def test_get_kwarg_literal(self, script_content, expect_val, expect_logs, what, tmpdir, caplog):
+        """
+        Basic tests for getting kwarg value from a literal.
+
+        Test cases only call setup() at top level, location of setup call is much more
+        important for tests with variables.
+        """
+        self._test_get_value(tmpdir, caplog, script_content, expect_val, expect_logs, what=what)
+
+    @pytest.mark.parametrize(
+        "version_val, expect_version",
+        [("1.0.alpha.1", "1.0a1"), (1, "1"), ((1, 0, "alpha", 1), "1.0a1")],
+    )
+    def test_get_version_special(self, version_val, expect_version, tmpdir, caplog):
+        """Test cases where version values get special handling."""
+        script_content = f"setup(version={version_val!r})"
+        expect_logs = [
+            "Found setup call on line 1",
+            "Pseudo-path: Module.body[0] -> Expr(#1).value",
+            f"setup kwarg 'version' is a literal: {version_val!r}",
+        ]
+        self._test_get_value(
+            tmpdir, caplog, script_content, expect_version, expect_logs, what="version"
+        )
+
+    @pytest.mark.parametrize(
+        "script_content, expect_val, expect_logs",
+        [
+            (
+                "setup({what}=foo)",
+                None,
+                [
+                    "Pseudo-path: Module.body[0] -> Expr(#1).value",
+                    "Variable 'foo' not found along the setup call branch",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    setup({what}=foo)
+
+                    foo = "bar"
+                    """
+                ),
+                None,
+                [
+                    "Pseudo-path: Module.body[0] -> Expr(#1).value",
+                    "Variable 'foo' not found along the setup call branch",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    if True:
+                        foo = "bar"
+
+                    setup({what}=foo)
+                    """
+                ),
+                None,
+                [
+                    "Pseudo-path: Module.body[1] -> Expr(#4).value",
+                    "Variable 'foo' not found along the setup call branch",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    foo = get_version()
+
+                    setup({what}=foo)
+                    """
+                ),
+                None,
+                [
+                    "Pseudo-path: Module.body[1] -> Expr(#3).value",
+                    "Variable cannot be resolved: 'foo' is not assigned to a literal expression",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    foo = None
+
+                    setup({what}=foo)
+                    """
+                ),
+                None,
+                ["Pseudo-path: Module.body[1] -> Expr(#3).value", "Found variable 'foo': None"],
+            ),
+            (
+                dedent(
+                    """\
+                    foo = "bar"
+
+                    setup({what}=foo)
+                    """
+                ),
+                "bar",
+                ["Pseudo-path: Module.body[1] -> Expr(#3).value", "Found variable 'foo': 'bar'"],
+            ),
+            (
+                dedent(
+                    """\
+                    foo = "bar"
+
+                    if True:
+                        setup({what}=foo)
+                    """
+                ),
+                "bar",
+                [
+                    "Pseudo-path: Module.body[1] -> If(#3).body[0] -> Expr(#4).value",
+                    "Found variable 'foo': 'bar'",
+                ],
+            ),
+            (
+                # Variable will be found only if it is in the same branch
+                dedent(
+                    """\
+                    if True:
+                        foo = "bar"
+                    else:
+                        setup({what}=foo)
+                    """
+                ),
+                None,
+                [
+                    "Pseudo-path: Module.body[0] -> If(#1).orelse[0] -> Expr(#4).value",
+                    "Variable 'foo' not found along the setup call branch",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    if True:
+                        foo = "bar"
+                        setup({what}=foo)
+                    """
+                ),
+                "bar",
+                [
+                    "Pseudo-path: Module.body[0] -> If(#1).body[1] -> Expr(#3).value",
+                    "Found variable 'foo': 'bar'",
+                ],
+            ),
+            (
+                # Try statements are kinda special, because not only do they have 3 bodies,
+                # they also have a list of 'handlers' (1 for each except clause)
+                dedent(
+                    """\
+                    try:
+                        pass
+                    except A:
+                        foo = "bar"
+                    except B:
+                        setup({what}=foo)
+                    else:
+                        pass
+                    finally:
+                        pass
+                    """
+                ),
+                None,
+                [
+                    (
+                        "Pseudo-path: Module.body[0] -> Try(#1).handlers[1] "
+                        "-> ExceptHandler(#5).body[0] -> Expr(#6).value"
+                    ),
+                    "Variable 'foo' not found along the setup call branch",
+                ],
+            ),
+            (
+                dedent(
+                    """\
+                    try:
+                        pass
+                    except A:
+                        pass
+                    except B:
+                        foo = "bar"
+                        setup({what}=foo)
+                    else:
+                        pass
+                    finally:
+                        pass
+                    """
+                ),
+                "bar",
+                [
+                    (
+                        "Pseudo-path: Module.body[0] -> Try(#1).handlers[1] "
+                        "-> ExceptHandler(#5).body[1] -> Expr(#7).value"
+                    ),
+                    "Found variable 'foo': 'bar'",
+                ],
+            ),
+            (
+                # setup() inside a FunctionDef is pretty much the same thing as setup()
+                # inside an If, except this could support late binding and doesn't
+                dedent(
+                    """\
+                    def f():
+                        setup({what}=foo)
+
+                    foo = "bar"
+
+                    f()
+                    """
+                ),
+                None,
+                [
+                    "Pseudo-path: Module.body[0] -> FunctionDef(#1).body[0] -> Expr(#2).value",
+                    "Variable 'foo' not found along the setup call branch",
+                ],
+            ),
+            (
+                # Variable defined closer should take precedence
+                dedent(
+                    """\
+                    foo = "baz"
+
+                    if True:
+                        foo = "bar"
+                        setup({what}=foo)
+                    """
+                ),
+                "bar",
+                [
+                    "Pseudo-path: Module.body[1] -> If(#3).body[1] -> Expr(#5).value",
+                    "Found variable 'foo': 'bar'",
+                ],
+            ),
+            (
+                # Search for setup() should be depth-first, i.e. find the first setup()
+                # call even if it is at a deeper level of indentation
+                dedent(
+                    """\
+                    if True:
+                        setup({what}=foo)
+
+                    foo = "bar"
+                    setup({what}=foo)
+                    """
+                ),
+                None,
+                [
+                    "Pseudo-path: Module.body[0] -> If(#1).body[0] -> Expr(#2).value",
+                    "Variable 'foo' not found along the setup call branch",
+                ],
+            ),
+            (
+                # Sanity check: all statements with bodies (except async def / async for)
+                dedent(
+                    """\
+                    foo = "bar"
+
+                    class C:
+                        def f():
+                            if True:
+                                for x in y:
+                                    while True:
+                                        with x:
+                                            try:
+                                                pass
+                                            except:
+                                                setup({what}=foo)
+                    """
+                ),
+                "bar",
+                [
+                    (
+                        "Pseudo-path: Module.body[1] -> ClassDef(#3).body[0] "
+                        "-> FunctionDef(#4).body[0] -> If(#5).body[0] -> For(#6).body[0] "
+                        "-> While(#7).body[0] -> With(#8).body[0] -> Try(#9).handlers[0] "
+                        "-> ExceptHandler(#11).body[0] -> Expr(#12).value"
+                    ),
+                    "Found variable 'foo': 'bar'",
+                ],
+            ),
+        ],
+    )
+    @pytest.mark.parametrize("what", ["name", "version"])
+    def test_get_kwarg_var(self, script_content, expect_val, expect_logs, what, tmpdir, caplog):
+        """Tests for getting kwarg value from a variable."""
+        lineno = next(
+            i + 1 for i, line in enumerate(script_content.splitlines()) if "setup" in line
+        )
+        logs = expect_logs + [
+            f"Found setup call on line {lineno}",
+            "setup kwarg '{what}' looks like a variable",
+            f"Backtracking up the AST from line {lineno} to find variable 'foo'",
+        ]
+        self._test_get_value(tmpdir, caplog, script_content, expect_val, logs, what=what)
+
+    @pytest.mark.parametrize(
+        "version_val, expect_version",
+        [("1.0.alpha.1", "1.0a1"), (1, "1"), ((1, 0, "alpha", 1), "1.0a1")],
+    )
+    def test_version_var_special(self, version_val, expect_version, tmpdir, caplog):
+        """Test that special version values are supported also for variables."""
+        script_content = dedent(
+            f"""\
+            foo = {version_val!r}
+
+            setup(version=foo)
+            """
+        )
+        expect_logs = [
+            "Found setup call on line 3",
+            "Pseudo-path: Module.body[1] -> Expr(#3).value",
+            "setup kwarg 'version' looks like a variable",
+            "Backtracking up the AST from line 3 to find variable 'foo'",
+            f"Found variable 'foo': {version_val!r}",
+        ]
+        self._test_get_value(
+            tmpdir, caplog, script_content, expect_version, expect_logs, what="version"
+        )
+
+    @pytest.mark.parametrize("what", ["name", "version"])
+    def test_kwarg_unsupported_expr(self, what, tmpdir, caplog):
+        """Value of kwarg is neither a literal nor a Name."""
+        script_content = f"setup({what}=get_version())"
+        expect_logs = [
+            "Found setup call on line 1",
+            "Pseudo-path: Module.body[0] -> Expr(#1).value",
+            f"setup kwarg '{what}' is an unsupported expression: Call",
+        ]
+        self._test_get_value(tmpdir, caplog, script_content, None, expect_logs, what=what)
