f647e9a67b99a9d53415baa2e41b6fe099a2cfa9
Athos Ribeiro <athos@redhat.com>
2020-09-06 18:13:51 +0200

    Add task to resolve and fetch pip dependencies
    Signed-off-by: Athos Ribeiro <athos@redhat.com>

--- tests/test_workers/test_tasks/test_pip.py
+++ tests/test_workers/test_tasks/test_pip.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 from unittest import mock
 
+import pytest
+
+from cachito.workers.config import get_worker_config
 from cachito.workers.tasks import pip
 
 
@@ -14,3 +17,54 @@ def test_cleanup_pip_request(mock_exec_script):
         "username": "cachito-pip-42",
     }
     mock_exec_script.assert_called_once_with("pip_cleanup", expected_payload)
+
+
+@pytest.mark.parametrize("with_cert", [True, False])
+@mock.patch("cachito.workers.tasks.pip.resolve_pip")
+@mock.patch("cachito.workers.tasks.pip.finalize_nexus_for_pip_request")
+@mock.patch("cachito.workers.tasks.pip.prepare_nexus_for_pip_request")
+@mock.patch("cachito.workers.tasks.pip.set_request_state")
+@mock.patch("cachito.workers.tasks.pip.update_request_with_deps")
+@mock.patch("cachito.workers.tasks.pip.update_request_with_package")
+@mock.patch("cachito.workers.tasks.pip.nexus.get_ca_cert")
+def test_fetch_pip_source(
+    mock_cert,
+    mock_update_pkg,
+    mock_update_deps,
+    mock_set_state,
+    mock_prepare_nexus,
+    mock_finalize_nexus,
+    mock_resolve,
+    with_cert,
+):
+    pkg_data = {
+        "package": {"name": "foo", "version": "1", "type": "pip"},
+        "dependencies": [{"name": "bar", "version": "2.0", "type": "pip", "dev": True}],
+    }
+    request = {"id": 1}
+    username = f"cachito-pip-{request['id']}"
+    password = "password"
+    repo_name = f"cachito-pip-hosted-{request['id']}"
+    config = get_worker_config()
+    nexus_url = config.cachito_nexus_url
+    index_base_url = nexus_url.replace("://", f"://{username}:{password}@")
+    env_vars = {
+        "PIP_INDEX_URL": {"value": f"{index_base_url}/repository/{repo_name}/", "kind": "literal"}
+    }
+    mock_cert.return_value = None
+    if with_cert:
+        mock_cert.return_value = "stub_cert"
+        env_vars["PIP_CERT"] = {"value": "stub_cert", "kind": "literal"}
+
+    mock_resolve.return_value = pkg_data
+    mock_finalize_nexus.return_value = password
+    mock_set_state.return_value = request
+
+    pip.fetch_pip_source(request["id"])
+
+    mock_update_pkg.assert_called_once_with(request["id"], pkg_data["package"], env_vars)
+    mock_update_deps.assert_called_once_with(
+        request["id"],
+        pkg_data["package"],
+        [{"name": "bar", "version": "2.0", "type": "pip", "dev": True}],
+    )
