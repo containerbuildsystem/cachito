bcea5234fd345b233959e2f5b4cded6f083aedde
Elina Akhmanova <eakhmano@redhat.com>
2020-09-30 15:17:43 +0200

    Integration tests: add pip local path test

--- tests/integration/test_pip_packages.py
+++ tests/integration/test_pip_packages.py
@@ -72,3 +72,33 @@ def test_all_pip_packages(env_name, test_env, tmpdir):
         deps_purls = []
     image_contents = [{"dependencies": deps_purls, "purl": purl, "sources": deps_purls}]
     utils.assert_content_manifest(client, completed_response.id, image_contents)
+
+
+def test_failing_pip_local_path(test_env):
+    """
+    Validate failing of the pip package request with local dependencies.
+
+    Process:
+    Send new request to the Cachito API
+    Send request to check status of existing request
+
+    Checks:
+    * Check that the request fails with expected error
+    """
+    env_data = test_env["pip_packages"]["local_path"]
+    client = utils.Client(test_env["api_url"], test_env["api_auth_type"], test_env.get("timeout"))
+    initial_response = client.create_new_request(
+        payload={
+            "repo": env_data["repo"],
+            "ref": env_data["ref"],
+            "pkg_managers": ["pip"],
+            # TODO: delete pip-dev-preview flag when
+            #  the pip package manager will be ready for production usage
+            "flags": ["pip-dev-preview"],
+        },
+    )
+    completed_response = client.wait_for_complete_request(initial_response)
+    assert completed_response.status == 200
+    assert completed_response.data["state"] == "failed"
+    error_msg = "Direct references with 'file' scheme are not supported"
+    assert error_msg in completed_response.data["state_reason"]
