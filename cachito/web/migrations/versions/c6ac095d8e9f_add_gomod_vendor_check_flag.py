b52dfca72d91bfe1f644de74c662d306d3319062
Adam Cmiel <acmiel@redhat.com>
2021-04-15 13:48:36 +0200

    Add the gomod-vendor-check flag
    CLOUDBLD-3282
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

---
+++ cachito/web/migrations/versions/c6ac095d8e9f_add_gomod_vendor_check_flag.py
@@ -0,0 +1,43 @@
+"""Add the gomod-vendor-check flag
+
+Revision ID: c6ac095d8e9f
+Revises: 07d89c5778f2
+Create Date: 2021-04-15 13:45:44.761284
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c6ac095d8e9f"
+down_revision = "07d89c5778f2"
+branch_labels = None
+depends_on = None
+
+
+flag_table = sa.Table(
+    "flag",
+    sa.MetaData(),
+    sa.Column("id", sa.Integer(), primary_key=True),
+    sa.Column("name", sa.String(), nullable=False),
+    sa.Column("active", sa.Boolean(), nullable=False, default=True),
+)
+
+
+def upgrade():
+    connection = op.get_bind()
+    res = connection.execute(
+        flag_table.select().where(flag_table.c.name == "gomod-vendor-check")
+    ).fetchone()
+    if res is None:
+        connection.execute(flag_table.insert().values(name="gomod-vendor-check", active=True))
+    else:
+        connection.execute(
+            flag_table.update().where(flag_table.c.name == "gomod-vendor-check").values(active=True)
+        )
+
+
+def downgrade():
+    connection = op.get_bind()
+    connection.execute(flag_table.update().values(name="gomod-vendor-check", active=False))
