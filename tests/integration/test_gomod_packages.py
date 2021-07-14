77ccc3ef88b38e986f463a0d78acc86338d16e7d
Daniel Cho <dacho@redhat.com>
2021-07-14 08:35:07 -0500

    Implement integration test for gomod-vendor-check
    CLOUDBLD-5578
    
    Add test function which fails with inconsistent vendor or empty vendor.
    Add test data for consistent vendor and no vendor.
    
    Signed-off-by: Daniel Cho <dacho@redhat.com>

--- tests/integration/test_gomod_packages.py
+++ tests/integration/test_gomod_packages.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import pytest
+
 from . import utils
 
 
@@ -32,3 +34,34 @@ def test_gomod_vendor_without_flag(test_env):
         )
     else:
         utils.assert_properly_completed_response(completed_response)
+
+
+@pytest.mark.parametrize("env_name", [("wrong_vendor"), ("empty_vendor")])
+def test_gomod_vendor_check_fail(env_name, test_env):
+    """
+    Validate failing of gomod vendor request with gomod-vendor-check flag and inconsistent vendor.
+
+    Checks:
+    * The request fails with expected error message
+    """
+    env_data = utils.load_test_data("gomod_vendor_check.yaml")[env_name]
+    client = utils.Client(test_env["api_url"], test_env["api_auth_type"], test_env.get("timeout"))
+    initial_response = client.create_new_request(
+        payload={
+            "repo": env_data["repo"],
+            "ref": env_data["ref"],
+            "flags": env_data["flags"],
+            "pkg_managers": env_data["pkg_managers"],
+        },
+    )
+    completed_response = client.wait_for_complete_request(initial_response)
+    assert completed_response.status == 200
+    assert completed_response.data["state"] == "failed"
+    error_msg = (
+        "The content of the vendor directory is not consistent with go.mod. "
+        "Run `go mod vendor` locally to fix this problem. See the logs for more details."
+    )
+    assert error_msg in completed_response.data["state_reason"], (
+        f"#{completed_response.id}: Request failed correctly, but with unexpected message: "
+        f"{completed_response.data['state_reason']}. Expected message was: {error_msg}"
+    )
