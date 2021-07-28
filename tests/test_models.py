e5b39cba253f503c698ea74ba19cb328a98367dd
Bruno Pimentel <bpimente@redhat.com>
2021-07-28 19:33:05 -0300

    Requests in any state will try loading the packages file
    This change aims to make it possible for the worker process
    to check the correct loading of the packages file by querying
    the API before setting the request's state to complete.
    
    Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from typing import Optional
-from unittest import mock
 
 import pytest
 
-from cachito.web.models import PackageManager, Request, RequestStateMapping
+from cachito.web.models import PackageManager
 
 
 @pytest.mark.parametrize(
@@ -17,38 +16,3 @@ def test_package_manager_get_by_name(name, expected, app, db, auth_env):
         assert expected == pkg_manager
     else:
         assert expected == pkg_manager.name
-
-
-class TestRequest:
-    def _create_request_object(self):
-        request = Request()
-        request.repo = "a_repo"
-        request.ref = "a_ref"
-        request.user_id = 1
-        request.submitted_by_id = 1
-        request.packages_count = 1
-        request.dependencies_count = 1
-
-        return request
-
-    @pytest.mark.parametrize(
-        "state, call_count",
-        [
-            [RequestStateMapping.in_progress.name, 0],
-            [RequestStateMapping.complete.name, 2],
-            [RequestStateMapping.failed.name, 0],
-            [RequestStateMapping.stale.name, 0],
-        ],
-    )
-    @mock.patch("cachito.common.packages_data.PackagesData.load")
-    def test_package_data_is_only_accessed_when_request_is_complete(
-        self, load_mock, state, call_count, app, auth_env
-    ):
-        request = self._create_request_object()
-        request.add_state(state, "Reason")
-
-        with app.test_request_context(environ_base=auth_env):
-            request.to_json()
-            request.content_manifest.to_json()
-
-        assert load_mock.call_count == call_count
