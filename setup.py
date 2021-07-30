2bd11abade53c4dd63e5be0b641ca61cf0f66ed3
Riccardo Pittau <elfosardo@gmail.com>
2021-07-30 14:44:17 +0200

    Add Python 3.6 as supported in setup.py
    In setup.py we specify python_requires=">=3.6"
    
    Also adding Python 3 only mention.
    
    Signed-off-by: Riccardo Pittau <elfosardo@gmail.com>

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
     },
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
