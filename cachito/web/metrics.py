2afe5675e16ab4d213399dc761b0b380f08f0f8e
Mike Kingsbury <mike.kingsbury@redhat.com>
2021-09-03 15:50:23 -0400

    Metric for requests in progress. First custom prometheus metric
    Signed-off-by: Mike Kingsbury <mike.kingsbury@redhat.com>

--- cachito/web/metrics.py
+++ cachito/web/metrics.py
@@ -1,9 +1,11 @@
 import os
 import socket
 
-from prometheus_client import multiprocess
+from prometheus_client import Gauge, Summary, multiprocess
 from prometheus_client.core import CollectorRegistry
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+
+cachito_metrics = {}
 
 
 def init_metrics(app):
@@ -24,3 +26,11 @@ def init_metrics(app):
         default_labels={"host": hostname}, group_by="endpoint", defaults_prefix="cachito_flask"
     )
     metrics.init_app(app)
+    gauge_state = Gauge(
+        "cachito_requests_count", "Requests in each state", ["state"], multiprocess_mode="livesum"
+    )
+    request_duration = Summary(
+        "cachito_request_duration_seconds", "Time spent in in_progress state"
+    )
+    cachito_metrics["gauge_state"] = gauge_state
+    cachito_metrics["request_duration"] = request_duration
