c17512b0ce6fb9e7aac8c07e374ef9d36744ad60
Milan Tichavský <mtichavs@redhat.com>
2022-06-01 16:50:14 +0200

    Add Gemfile.lock parsing functionality
    SPMM-9896
    
    As of right now, it supports only Gemfile.lock files with ruby as its
    only platform.
    
    Signed-off-by: Milan Tichavský <mtichavs@redhat.com>

--- cachito/workers/pkg_managers/rubygems.py
+++ cachito/workers/pkg_managers/rubygems.py
@@ -1,10 +1,29 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
 import logging
+import re
+from dataclasses import dataclass
+from pathlib import Path
 
-from cachito.errors import CachitoError
+from gemlock_parser.gemfile_lock import GemfileLockParser
+
+from cachito.errors import CachitoError, ValidationError
 from cachito.workers import nexus
 from cachito.workers.errors import NexusScriptError
 
+GIT_REF_FORMAT = re.compile(r"^[a-fA-F0-9]{40}$")
+PLATFORMS_RUBY = re.compile(r"^PLATFORMS\n {2}ruby\n\n", re.MULTILINE)
+
 log = logging.getLogger(__name__)
+
+
+@dataclass
+class GemMetadata:
+    """Gem metadata."""
+
+    name: str
+    version: str
+    type: str
+    source: str
 
 
 def prepare_nexus_for_rubygems_request(rubygems_repo_name, raw_repo_name):
@@ -25,3 +44,95 @@ def prepare_nexus_for_rubygems_request(rubygems_repo_name, raw_repo_name):
     except NexusScriptError:
         log.exception("Failed to execute the script %s", script_name)
         raise CachitoError("Failed to prepare Nexus for Cachito to stage Rubygems content")
+
+
+def parse_gemlock(source_dir, gemlock_path):
+    """Parse dependencies from Gemfile.lock.
+
+    :param Path source_dir: the full path to the project directory
+    :param Path gemlock_path: the full path to Gemfile.lock
+    :return: list of Gems
+    """
+    if not gemlock_path.is_file():
+        raise ValidationError(
+            f"Gemfile.lock at path {gemlock_path} does not exist or is not a regular file."
+        )
+
+    _validate_gemlock_platforms(gemlock_path)
+
+    dependencies = []
+    all_gems = GemfileLockParser(str(gemlock_path)).all_gems
+    for gem in all_gems.values():
+        _validate_gem_metadata(gem, source_dir, gemlock_path.parent)
+        source = gem.remote if gem.type != "PATH" else gem.path
+        dependencies.append(GemMetadata(gem.name, gem.version, gem.type, source))
+
+    return dependencies
+
+
+def _validate_gemlock_platforms(gemlock_path):
+    """Make sure Gemfile.lock contains only one platform - ruby."""
+    with open(gemlock_path) as f:
+        contents = f.read()
+
+    if not PLATFORMS_RUBY.search(contents):
+        msg = "PLATFORMS section of Gemfile.lock has to contain one and only platform - ruby."
+        raise ValidationError(msg)
+
+
+def _validate_gem_metadata(gem, source_dir, gemlock_dir):
+    """Validate parsed Gem.
+
+    While individual gems may contain platform information, this function doesn't check it,
+    because it expects the Gemfile.lock to be ruby platform specific.
+    :param Gem gem: gem with information parsed from Gemfile.lock
+    :param Path source_dir: the full path to the project root
+    :param Path gemlock_dir: the root directory containing Gemfile.lock
+    :raise: ValidationError
+    """
+    if gem.name is None or gem.version is None:
+        raise ValidationError("Unspecified name or version of a RubyGem.")
+
+    if gem.type == "GEM":
+        if gem.remote != "https://rubygems.org/":
+            raise ValidationError(
+                "Cachito supports only https://rubygems.org/ as a remote for Ruby GEM dependencies."
+            )
+    elif gem.type == "GIT":
+        if not gem.remote.startswith("https://"):
+            raise ValidationError("All Ruby GIT dependencies have to use HTTPS protocol.")
+        if not GIT_REF_FORMAT.match(gem.version):
+            msg = (
+                f"No git ref for gem: {gem.name} (expected 40 hexadecimal characters, "
+                f"got: {gem.version})."
+            )
+            raise ValidationError(msg)
+    elif gem.type == "PATH":
+        _validate_path_dependency_dir(gem, source_dir, gemlock_dir)
+    else:
+        raise ValidationError("Gemfile.lock contains unsupported dependency type.")
+
+
+def _validate_path_dependency_dir(gem, project_root, gemlock_dir):
+    """Validate path of PATH dependency.
+
+    :param gem: validated gem
+    :param project_root: project root directory
+    :param gemlock_dir: absolute path to Gemfile.lock parent directory
+    """
+    dependency_dir = gemlock_dir / Path(gem.path)
+    try:
+        dependency_dir = dependency_dir.resolve(strict=True)
+        dependency_dir.relative_to(project_root.resolve())
+    except FileNotFoundError:
+        raise ValidationError(
+            f"PATH dependency {str(gem.name)} references a non-existing path: "
+            f"{str(dependency_dir)}."
+        )
+    except RuntimeError:
+        raise ValidationError(
+            f"Path of PATH dependency {str(gem.name)} contains an infinite loop: "
+            f"{str(dependency_dir)}."
+        )
+    except ValueError:
+        raise ValidationError(f"{str(dependency_dir)} is not a subpath of {str(project_root)}")
--- tests/test_workers/test_pkg_managers/test_rubygems.py
+++ tests/test_workers/test_pkg_managers/test_rubygems.py
@@ -1,10 +1,14 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+from pathlib import Path
+from textwrap import dedent
 from unittest import mock
 
 import pytest
 
-from cachito.errors import CachitoError
+from cachito.errors import CachitoError, ValidationError
 from cachito.workers.errors import NexusScriptError
 from cachito.workers.pkg_managers import rubygems
+from cachito.workers.pkg_managers.rubygems import GemMetadata, parse_gemlock
 
 
 class TestNexus:
@@ -35,3 +39,342 @@ class TestNexus:
             rubygems.prepare_nexus_for_rubygems_request(
                 "cachito-rubygems-hosted-1", "cachito-rubygems-raw-1"
             )
+
+
+class TestGemlockParsing:
+    @pytest.mark.parametrize(
+        "file_contents, expected_dependencies",
+        (
+            # GEM dependency
+            (
+                dedent(
+                    """
+                    GEM
+                      remote: https://rubygems.org/
+                      specs:
+                        zeitwerk (2.5.4)
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      zeitwerk
+                    """
+                ),
+                [GemMetadata("zeitwerk", "2.5.4", "GEM", "https://rubygems.org/")],
+            ),
+            # GIT dependency
+            (
+                dedent(
+                    """
+                    GIT
+                      remote: https://github.com/3scale/ci_reporter_shell.git
+                      revision: 30b30d655512891f56463e5f1fa125ea1f2df886
+                      specs:
+                        ci_reporter_shell (0.1.0)
+                          ci_reporter (~> 2.0)
+
+                    GEM
+                      remote: https://rubygems.org/
+                      specs:
+                        builder (3.2.4)
+                        ci_reporter (2.0.0)
+                          builder (>= 2.1.2)
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      ci_reporter_shell!
+                    """
+                ),
+                [
+                    GemMetadata(
+                        "ci_reporter_shell",
+                        "30b30d655512891f56463e5f1fa125ea1f2df886",
+                        "GIT",
+                        "https://github.com/3scale/ci_reporter_shell.git",
+                    ),
+                    GemMetadata("ci_reporter", "2.0.0", "GEM", "https://rubygems.org/"),
+                    GemMetadata("builder", "3.2.4", "GEM", "https://rubygems.org/"),
+                ],
+            ),
+        ),
+    )
+    def test_parsing_of_valid_cases(self, file_contents, expected_dependencies, tmpdir):
+        gemfile_lock = tmpdir.join("Gemfile.lock")
+        gemfile_lock.write(file_contents)
+
+        dependencies = parse_gemlock(Path(gemfile_lock).parent, Path(gemfile_lock))
+
+        assert len(dependencies) == len(expected_dependencies)
+        for dep, expected_dep in zip(dependencies, expected_dependencies):
+            assert expected_dep == dep
+
+    def test_gemlock_not_a_file(self, tmpdir):
+        gemfile_lock = tmpdir.join("Gemfile.lock")
+
+        with pytest.raises(ValidationError) as exc_info:
+            parse_gemlock(Path(gemfile_lock).parent, Path(gemfile_lock))
+
+        expected_msg = (
+            f"Gemfile.lock at path {gemfile_lock} does not exist or is not a regular file."
+        )
+        assert expected_msg == str(exc_info.value)
+
+    def test_parsing_of_valid_path_dependency(self, tmpdir):
+        gemlock_contents = dedent(
+            """
+            PATH
+              remote: local/zeitwerk
+              specs:
+                zeitwerk (2.6.0)
+
+            GEM
+              remote: https://rubygems.org/
+              specs:
+
+            PLATFORMS
+              ruby
+
+            DEPENDENCIES
+              zeitwerk!
+            """
+        )
+        expected_dependencies = [GemMetadata("zeitwerk", "2.6.0", "PATH", "local/zeitwerk")]
+
+        gemfile_lock = tmpdir.join("Gemfile.lock")
+        gemfile_lock.write(gemlock_contents)
+        tmpdir.mkdir("local").mkdir("zeitwerk")
+
+        dependencies = parse_gemlock(Path(gemfile_lock).parent, Path(gemfile_lock))
+
+        assert len(dependencies) == len(expected_dependencies)
+        for dep, expected_dep in zip(dependencies, expected_dependencies):
+            assert expected_dep == dep
+
+    @pytest.mark.parametrize(
+        "file_contents, expected_error",
+        (
+            (
+                dedent(
+                    """
+                    GEM
+                      remote: https://rubygems.org/
+                      specs:
+                        zeitwerk (2.5.4)
+
+                    DEPENDENCIES
+                      zeitwerk
+                    """
+                ),
+                "PLATFORMS section of Gemfile.lock has to contain one and only platform - ruby.",
+            ),
+            (
+                dedent(
+                    """
+                    GEM
+                      remote: http://rubygems.org/
+                      specs:
+                        zeitwerk (2.5.4)
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      zeitwerk
+                    """
+                ),
+                "Cachito supports only https://rubygems.org/ as a remote for Ruby GEM "
+                "dependencies.",
+            ),
+            (
+                dedent(
+                    """
+                    GIT
+                      remote: http://github.com/3scale/json-schema.git
+                      revision: 26487618a68443e94d623bb585cb464b07d36702
+                      specs:
+                        json-schema (3.0.0)
+                          addressable (>= 2.4)
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      json-schema!
+                    """
+                ),
+                "All Ruby GIT dependencies have to use HTTPS protocol.",
+            ),
+            (
+                dedent(
+                    """
+                    GIT
+                      remote: https://github.com/3scale/json-schema.git
+                      revision: xxx
+                      specs:
+                        json-schema (3.0.0)
+                          addressable (>= 2.4)
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      json-schema!
+                    """
+                ),
+                "No git ref for gem: json-schema (expected 40 hexadecimal characters, got: xxx).",
+            ),
+            (
+                dedent(
+                    """
+                    UNSUPPORTED
+                      remote: vendor/active-docs
+                      specs:
+                        active-docs (1.0.0)
+                          railties (> 3.1)
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      active-docs!
+                    """
+                ),
+                "Gemfile.lock contains unsupported dependency type.",
+            ),
+            (
+                dedent(
+                    """
+                    GEM
+                      remote: https://rubygems.org/
+                      specs:
+                        zeitwerk
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      zeitwerk
+                    """
+                ),
+                "Unspecified name or version of a RubyGem.",
+            ),
+            (
+                dedent(
+                    """
+                    GEM
+                      remote: https://rubygems.org/
+                      specs:
+                        zeitwerk (2.5.4)
+
+                    PLATFORMS
+                      ruby
+                      x86_64-linux
+
+                    DEPENDENCIES
+                      zeitwerk
+                    """
+                ),
+                "PLATFORMS section of Gemfile.lock has to contain one and only platform - ruby.",
+            ),
+        ),
+    )
+    def test_parsing_of_invalid_cases(self, file_contents, expected_error, tmpdir):
+        """Test the invalid use cases of dependencies in a Gemfile.lock file."""
+        gemfile_lock = tmpdir.join("Gemfile.lock")
+        gemfile_lock.write(file_contents)
+
+        with pytest.raises(ValidationError) as exc_info:
+            parse_gemlock(Path(gemfile_lock).parent, Path(gemfile_lock))
+
+        assert expected_error == str(exc_info.value)
+
+    @pytest.mark.parametrize(
+        "file_contents, expected_error",
+        (
+            (
+                dedent(
+                    """
+                    PATH
+                      remote: vendor/active-docs
+                      specs:
+                        active-docs (1.0.0)
+
+                    PLATFORMS
+                      ruby
+
+                    DEPENDENCIES
+                      active-docs!
+                    """
+                ),
+                "PATH dependency active-docs references a non-existing path: ",
+            ),
+        ),
+    )
+    def test_parsing_of_invalid_paths(self, file_contents, expected_error, tmpdir):
+        """Test the invalid path definition in a Gemfile.lock file."""
+        gemfile_lock = tmpdir.join("Gemfile.lock")
+        gemfile_lock.write(file_contents)
+
+        with pytest.raises(ValidationError) as exc_info:
+            parse_gemlock(Path(gemfile_lock).parent, Path(gemfile_lock))
+
+        assert expected_error in str(exc_info.value)
+
+    def test_path_is_not_a_subpath_absolute(self, tmpdir):
+        active_docs = Path(tmpdir).joinpath("active-docs")
+        active_docs.mkdir()
+
+        gemlock_contents = dedent(
+            f"""
+            PATH
+              remote: {str(active_docs)}
+              specs:
+                active-docs (1.0.0)
+
+            PLATFORMS
+              ruby
+
+            DEPENDENCIES
+              active-docs!
+            """
+        )
+        gemfile_lock = tmpdir.mkdir("gem").join("Gemfile.lock")
+        gemfile_lock.write(gemlock_contents)
+        gemfile_lock = Path(gemfile_lock)
+
+        with pytest.raises(ValueError) as exc_info:
+            parse_gemlock(gemfile_lock.parent, gemfile_lock)
+
+        expected_msg = f"{str(active_docs)} is not a subpath of {str(gemfile_lock.parent)}"
+        assert expected_msg in str(exc_info.value)
+
+    def test_paths_is_not_a_subpath_relative_path(self, tmpdir):
+        gemlock_contents = dedent(
+            """
+            PATH
+              remote: ../active-docs
+              specs:
+                active-docs (1.0.0)
+
+            PLATFORMS
+              ruby
+
+            DEPENDENCIES
+              active-docs!
+            """
+        )
+        gemfile_lock = tmpdir.mkdir("gem").join("Gemfile.lock")
+        gemfile_lock.write(gemlock_contents)
+        gemfile_lock = Path(gemfile_lock)
+
+        active_docs = Path(tmpdir).joinpath("active-docs")
+        active_docs.mkdir()
+
+        with pytest.raises(ValueError) as exc_info:
+            parse_gemlock(gemfile_lock.parent, gemfile_lock)
+
+        expected_msg = f"{str(active_docs)} is not a subpath of {str(gemfile_lock.parent)}"
+        assert expected_msg in str(exc_info.value)
