2f4063c0133910c3e58f3c989bb5e2a7b41dac6f
Milan Tichavský <mtichavs@redhat.com>
2022-08-04 09:58:34 +0200

    Add database migration enabling RubyGems
    SPMM-9909
    
    Signed-off-by: Milan Tichavský <mtichavs@redhat.com>

--- cachito/web/models.py
+++ cachito/web/models.py
@@ -181,7 +181,7 @@ def _validate_request_package_configs(request_kwargs, pkg_managers_names):
             + ", ".join(invalid_package_managers)
         )
 
-    supported_packages_configs = {"npm", "pip", "gomod", "yarn"}
+    supported_packages_configs = {"npm", "pip", "gomod", "yarn", "rubygems"}
     unsupported_packages_managers = packages_configs.keys() - supported_packages_configs
     if unsupported_packages_managers:
         raise ValidationError(
@@ -195,6 +195,7 @@ def _validate_request_package_configs(request_kwargs, pkg_managers_names):
         "pip": {"path", "requirements_build_files", "requirements_files"},
         "gomod": {"path"},
         "yarn": {"path"},
+        "rubygems": {"path"},
     }
     for pkg_manager, packages_config in packages_configs.items():
         invalid_format_error = (
