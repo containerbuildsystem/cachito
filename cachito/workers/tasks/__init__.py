a22d283f5633e478577fc91c1cccc0fe64dbc450
Ladislav Kolacek <lkolacek@redhat.com>
2022-09-09 19:14:31 +0200

    Cleanup repository for Cachi2
    Cachi2 will share code parts with the Cachito version1,
    but these parts will be added continuously in following PRs.
    
    Signed-off-by: Ladislav Kolacek <lkolacek@redhat.com>

--- cachito/workers/tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-from cachito.workers.tasks.general import *  # noqa: F401, F403
-from cachito.workers.tasks.gitsubmodule import *  # noqa: F401, F403
-from cachito.workers.tasks.gomod import *  # noqa: F401, F403
-from cachito.workers.tasks.npm import *  # noqa: F401, F403
-from cachito.workers.tasks.pip import *  # noqa: F401, F403
-from cachito.workers.tasks.rubygems import *  # noqa: F401, F403
-from cachito.workers.tasks.yarn import *  # noqa: F401, F403
