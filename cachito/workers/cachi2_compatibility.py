7059d77b7924bf1cff6ed566081188f275405302
Adam Cmiel <acmiel@redhat.com>
2023-02-10 12:08:21 +0100

    Implement part of gomod functionality using cachi2
    Passes a few tests
    
        tox -e integration -- tests/integration -k 'gomod or go_generate'
    
    The failures are mostly:
    
    * strict behavior where Cachito was non-strict
        * some can still be configured, some cannot
    * different error messages
    
    A missing failure is the cached_dependencies test, because we can't run
    it locally. It would fail, because the current implementation doesn't
    use Athens.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

---
+++ cachito/workers/cachi2_compatibility.py
@@ -0,0 +1,159 @@
+import logging
+from pathlib import Path
+from typing import Any, Literal, Optional
+
+import pydantic
+from cachi2.core import errors as cachi2_errors
+from cachi2.core.models.input import Request as Cachi2Request
+from cachi2.core.models.output import Dependency, Package, PipDependency, RequestOutput
+from cachi2.core.package_managers import gomod, pip
+
+from cachito.common.packages_data import PackagesData
+from cachito.errors import (
+    CachitoError,
+    ClientError,
+    GoModError,
+    InvalidFileFormat,
+    InvalidRepoStructure,
+    NetworkError,
+    ServerError,
+    UnsupportedFeature,
+    ValidationError,
+)
+from cachito.workers.paths import RequestBundleDir
+from cachito.workers.pkg_managers.general import update_request_env_vars
+
+__all__ = ["Cachi2Adapter"]
+
+log = logging.getLogger(__name__)
+
+JSONObject = dict[str, Any]
+
+
+class Cachi2Adapter:
+    def __init__(
+        self,
+        request_json: JSONObject,
+        request_bundle: RequestBundleDir,
+        package_manager: Literal["gomod", "pip"],
+    ) -> None:
+        self.request_json = request_json
+        self.request_bundle = request_bundle
+        self.package_manager = package_manager
+        # TODO: set config
+        # TODO: config is global in cachi2
+
+    def run_package_manager(
+        self,
+        package_configs_for_package_manager: list[JSONObject],
+        dependency_replacements_for_package_manager: Optional[list[JSONObject]] = None,
+    ) -> RequestOutput:
+
+        if self.package_manager == "gomod":
+            fetch_fn = gomod.fetch_gomod_source
+        elif self.package_manager == "pip":
+            fetch_fn = pip.fetch_pip_source
+        else:
+            raise RuntimeError("How did you get here? Typing-wise, this is impossible.")
+
+        cachi2_request = self._to_cachi2_request(
+            package_configs_for_package_manager,
+            dependency_replacements_for_package_manager,
+        )
+        try:
+            cachi2_output = fetch_fn(cachi2_request)
+        except cachi2_errors.Cachi2Error as e:
+            log.error(e.friendly_msg())
+            raise _to_cachito_error(e) from e
+        except pydantic.ValidationError:
+            log.exception("The Cachi2 backend failed to produce valid output")
+            raise ServerError("The Cachi2 backend failed to produce valid output")
+
+        return cachi2_output
+
+    def update_request_env_vars(self, cachi2_output: RequestOutput) -> None:
+        env_vars = {
+            ev.name: {"value": ev.value, "kind": ev.kind}
+            for ev in cachi2_output.environment_variables
+        }
+        update_request_env_vars(self.request_json["id"], env_vars)
+
+    def update_request_config_files(self, cachi2_output: RequestOutput) -> None:
+        raise ValueError(
+            "Cachi2's project files are not compatible with Cachito's config files. "
+            "Add them differently please."
+        )
+
+    def update_request_packages(self, cachi2_output: RequestOutput) -> PackagesData:
+        packages_data = PackagesData()
+
+        def to_package_dict(pkg: Package) -> JSONObject:
+            return {"type": pkg.type, "name": pkg.name, "version": pkg.version}
+
+        def to_dependency_dict(dep: Dependency) -> JSONObject:
+            attrs = {"type": dep.type, "name": dep.name, "version": dep.version}
+            if isinstance(dep, PipDependency):
+                attrs["dev"] = dep.dev
+            return attrs
+
+        for package in cachi2_output.packages:
+            packages_data.add_package(
+                pkg_info=to_package_dict(package),
+                path=str(package.path),
+                deps=[to_dependency_dict(dep) for dep in package.dependencies],
+            )
+
+        packages_file = getattr(self.request_bundle, f"{self.package_manager}_packages_data")
+        packages_data.write_to_file(packages_file)
+        return packages_data
+
+    @property
+    def _cachi2_source_dir(self) -> Path:
+        return self.request_bundle.source_root_dir
+
+    @property
+    def _cachi2_output_dir(self) -> Path:
+        return self.request_bundle
+
+    def _to_cachi2_request(
+        self,
+        package_configs_for_package_manager: list[JSONObject],
+        dependency_replacements_for_package_manager: Optional[list[JSONObject]] = None,
+    ) -> Cachi2Request:
+
+        packages = [
+            {"type": self.package_manager, **package}
+            for package in package_configs_for_package_manager
+        ]
+        try:
+            cachi2_request = Cachi2Request.parse_obj(
+                {
+                    "source_dir": self._cachi2_source_dir,
+                    "output_dir": self._cachi2_output_dir,
+                    "packages": packages,
+                    "flags": self.request_json["flags"],
+                    "dep_replacements": dependency_replacements_for_package_manager,
+                }
+            )
+        except pydantic.ValidationError:
+            log.exception("Failed to pass Cachito request to the Cachi2 backend")
+            raise ServerError("Failed to pass Cachito request to the Cachi2 backend")
+
+        return cachi2_request
+
+
+def _to_cachito_error(e: cachi2_errors.Cachi2Error) -> CachitoError | ClientError | ServerError:
+    if isinstance(e, cachi2_errors.InvalidInput):
+        return ValidationError(str(e))
+    if isinstance(e, cachi2_errors.UnexpectedFormat):
+        return InvalidFileFormat(str(e))
+    if isinstance(e, cachi2_errors.PackageRejected):
+        # TODO: will often be wrong
+        return InvalidRepoStructure(str(e))
+    if isinstance(e, cachi2_errors.UnsupportedFeature):
+        return UnsupportedFeature(str(e))
+    if isinstance(e, cachi2_errors.FetchError):
+        return NetworkError(str(e))
+    if isinstance(e, cachi2_errors.GoModError):
+        return GoModError(str(e))
+    return CachitoError(str(e))
--- cachito/workers/tasks/gomod.py
+++ cachito/workers/tasks/gomod.py
@@ -4,50 +4,16 @@ import os
 from pathlib import Path
 
 from cachito.common.packages_data import PackagesData
-from cachito.errors import (
-    FileAccessError,
-    GoModError,
-    InvalidRepoStructure,
-    InvalidRequestData,
-    UnsupportedFeature,
-)
+from cachito.errors import InvalidRepoStructure, InvalidRequestData
 from cachito.workers import run_cmd
-from cachito.workers.config import get_worker_config
+from cachito.workers.cachi2_compatibility import Cachi2Adapter
 from cachito.workers.paths import RequestBundleDir
-from cachito.workers.pkg_managers.general import update_request_env_vars
-from cachito.workers.pkg_managers.gomod import (
-    match_parent_module,
-    path_to_subpackage,
-    resolve_gomod,
-)
+from cachito.workers.pkg_managers import gomod
 from cachito.workers.tasks.celery import app
 from cachito.workers.tasks.utils import get_request, runs_if_request_in_progress, set_request_state
 
 __all__ = ["fetch_gomod_source"]
 log = logging.getLogger(__name__)
-
-
-def _find_missing_gomod_files(bundle_dir, subpaths):
-    """
-    Find all go modules with missing gomod files.
-
-    These files will need to be present in order for the package manager to proceed with
-    fetching the package sources.
-
-    :param RequestBundleDir bundle_dir: the ``RequestBundleDir`` object for the request
-    :param list subpaths: a list of subpaths in the source repository of gomod packages
-    :return: a list containing all non-existing go.mod files across subpaths
-    :rtype: list
-    """
-    invalid_gomod_files = []
-    for subpath in subpaths:
-        bundle_dir_subpath = bundle_dir.app_subpath(subpath)
-        package_gomod_rel_path = bundle_dir_subpath.relpath(bundle_dir_subpath.go_mod_file)
-        log.debug("Testing for go mod file in {}".format(package_gomod_rel_path))
-        if not bundle_dir_subpath.go_mod_file.exists():
-            invalid_gomod_files.append(package_gomod_rel_path)
-
-    return invalid_gomod_files
 
 
 def _is_workspace(repo_root: Path, subpath: str):
@@ -87,7 +53,7 @@ def _fail_if_parent_replacement_not_included(packages_json_data: PackagesData) -
     for package in packages_json_data.packages:
         for dependency in package.get("dependencies", []):
             if dependency["version"] and ".." in Path(dependency["version"]).parts:
-                pkg_module_name = match_parent_module(package["name"], modules)
+                pkg_module_name = gomod.match_parent_module(package["name"], modules)
                 if pkg_module_name is None:
                     # This should be impossible
                     raise RuntimeError(
@@ -97,7 +63,7 @@ def _fail_if_parent_replacement_not_included(packages_json_data: PackagesData) -
                 dep_normpath = os.path.normpath(
                     os.path.join(pkg_module_name, dependency["version"])
                 )
-                dep_module_name = match_parent_module(dep_normpath, modules)
+                dep_module_name = gomod.match_parent_module(dep_normpath, modules)
                 if dep_module_name is None:
                     raise InvalidRequestData(
                         (
@@ -128,84 +94,35 @@ def fetch_gomod_source(request_id, dep_replacements=None, package_configs=None):
     version_output = run_cmd(["go", "version"], {})
     log.info(f"Go version: {version_output.strip()}")
 
-    config = get_worker_config()
-    if package_configs is None:
-        package_configs = []
+    # config = get_worker_config()
+    if not package_configs:
+        package_configs = [{"path": "."}]
 
     bundle_dir: RequestBundleDir = RequestBundleDir(request_id)
-    subpaths = [os.path.normpath(c["path"]) for c in package_configs if c.get("path")]
+    subpaths = [os.path.normpath(c["path"]) for c in package_configs]
 
     if not subpaths:
         # Default to the root of the application source
         subpaths = [os.curdir]
 
+    # TODO: this is supposed to be in cachi2 of course
     _fail_if_bundle_dir_has_workspaces(bundle_dir, subpaths)
 
-    invalid_gomod_files = _find_missing_gomod_files(bundle_dir, subpaths)
-    if invalid_gomod_files:
-        invalid_files_print = "; ".join(invalid_gomod_files)
-        file_suffix = "s" if len(invalid_gomod_files) > 1 else ""
+    # TODO: this garbage
+    # missing gomod files is supported if there is only one path referenced
+    # if config.cachito_gomod_ignore_missing_gomod_file and len(subpaths) == 1:
+    #     log.warning("go.mod file missing for request at %s", invalid_files_print)
+    #     return
 
-        # missing gomod files is supported if there is only one path referenced
-        if config.cachito_gomod_ignore_missing_gomod_file and len(subpaths) == 1:
-            log.warning("go.mod file missing for request at %s", invalid_files_print)
-            return
+    set_request_state(request_id, "in_progress", "Fetching gomod dependencies")
 
-        raise FileAccessError(
-            "The {} file{} must be present for the gomod package manager".format(
-                invalid_files_print.strip(), file_suffix
-            )
-        )
+    cachi2_adapter = Cachi2Adapter(
+        request_json=get_request(request_id),
+        request_bundle=bundle_dir,
+        package_manager="gomod",
+    )
+    cachi2_output = cachi2_adapter.run_package_manager(package_configs, dep_replacements)
+    cachi2_adapter.update_request_env_vars(cachi2_output)
+    packages_data = cachi2_adapter.update_request_packages(cachi2_output)
 
-    if len(subpaths) > 1 and dep_replacements:
-        raise UnsupportedFeature(
-            "Dependency replacements are only supported for a single go module path."
-        )
-
-    env_vars = {
-        "GOCACHE": {"value": "deps/gomod", "kind": "path"},
-        "GOPATH": {"value": "deps/gomod", "kind": "path"},
-        "GOMODCACHE": {"value": "deps/gomod/pkg/mod", "kind": "path"},
-    }
-    env_vars.update(config.cachito_default_environment_variables.get("gomod", {}))
-    update_request_env_vars(request_id, env_vars)
-
-    packages_json_data = PackagesData()
-
-    for i, subpath in enumerate(subpaths):
-        log.info(
-            "Fetching the gomod dependencies for request %d in subpath %s", request_id, subpath
-        )
-        set_request_state(
-            request_id,
-            "in_progress",
-            f'Fetching the gomod dependencies at the "{subpath}" directory',
-        )
-        request = get_request(request_id)
-        gomod_source_path = str(bundle_dir.app_subpath(subpath).source_dir)
-        try:
-            gomod = resolve_gomod(
-                gomod_source_path, request, dep_replacements, bundle_dir.source_dir
-            )
-        except GoModError:
-            log.exception("Failed to fetch gomod dependencies for request %d", request_id)
-            raise
-
-        module_info = gomod["module"]
-
-        packages_json_data.add_package(module_info, subpath, gomod["module_deps"])
-
-        # add package deps
-        for package in gomod["packages"]:
-            pkg_info = package["pkg"]
-            package_subpath = _package_subpath(module_info["name"], pkg_info["name"], subpath)
-            packages_json_data.add_package(pkg_info, package_subpath, package.get("pkg_deps", []))
-
-    _fail_if_parent_replacement_not_included(packages_json_data)
-    packages_json_data.write_to_file(bundle_dir.gomod_packages_data)
-
-
-def _package_subpath(module_name: str, package_name: str, module_subpath: str) -> str:
-    """Get path from repository root to a package inside a module."""
-    subpath = path_to_subpackage(module_name, package_name)
-    return os.path.normpath(os.path.join(module_subpath, subpath))
+    _fail_if_parent_replacement_not_included(packages_data)
