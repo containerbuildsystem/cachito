312a3fbfc3a8243083f250f9750b8793060c0042
Felipe de Almeida <fdealmei@redhat.com>
2023-03-13 16:27:21 -0300

    Add step to make a new commit when running test_private_repos
    To prevent cachito from caching private source code and serving it without trying to access the repository.
    
    STONEBLD-661
    
    Signed-off-by: Felipe de Almeida <fdealmei@redhat.com>

--- tests/integration/test_private_repos.py
+++ tests/integration/test_private_repos.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import os
+import random
+import string
 from pathlib import Path
 from typing import Any
 
@@ -16,6 +18,9 @@ def test_private_repos(env_package: str, test_env: dict[str, Any], tmp_path: Pat
     Validate a cachito request with no package managers to a private repo.
 
     Process:
+    Create new commit at "cachito-no-package-manager-private" repo
+        (To prevent cachito from caching private source code and serving
+        it without trying to access the repository, more info: STONEBLD-661)
     Send new request to the Cachito API
     Send request to check status of existing request
 
@@ -31,7 +36,8 @@ def test_private_repos(env_package: str, test_env: dict[str, Any], tmp_path: Pat
     test_data = utils.load_test_data("private_repo_packages.yaml")
     private_repo_test_envs = test_data["private_repo_test_envs"]
     env_data = test_data[env_package]
-    is_supported_env = any(x in str(os.environ.get("JOB_NAME")) for x in private_repo_test_envs)
+    job_name = str(os.environ.get("JOB_NAME"))
+    is_supported_env = any(x in job_name for x in private_repo_test_envs)
     if not is_supported_env:
         pytest.skip(
             (
@@ -41,26 +47,50 @@ def test_private_repos(env_package: str, test_env: dict[str, Any], tmp_path: Pat
             )
         )
 
-    client = utils.Client(test_env["api_url"], test_env["api_auth_type"], test_env.get("timeout"))
-    payload = {
-        "repo": env_data["repo"],
-        "ref": env_data["ref"],
-        "pkg_managers": [],
-        "flags": ["include-git-dir"],
-    }
+    repo = Repo.clone_from(test_data["private_repo_ssh"]["repo"], tmp_path)
 
-    initial_response = client.create_new_request(payload=payload)
-    completed_response = client.wait_for_complete_request(initial_response)
+    repo.config_writer().set_value("user", "name", test_env["git_user"]).release()
+    repo.config_writer().set_value("user", "email", test_env["git_email"]).release()
 
-    utils.assert_properly_completed_response(completed_response)
-    assert completed_response.data["packages"] == []
-    assert completed_response.data["dependencies"] == []
+    generated_suffix = "".join(
+        random.choice(string.ascii_letters + string.digits) for x in range(10)
+    )
+    branch_name = f"tmp-branch-{generated_suffix}"
 
-    client.download_and_extract_archive(completed_response.id, tmp_path)
-    source_path = tmp_path / f"download_{str(completed_response.id)}"
-    repo = Repo(source_path / "app")
-    assert repo.head.commit.hexsha == env_data["ref"]
-    assert not repo.git.diff()
-    assert not os.listdir(source_path / "deps")
+    try:
+        repo.create_head(branch_name).checkout()
 
-    utils.assert_content_manifest(client, completed_response.id, [])
+        message = "Committed by Cachito integration test (test_private_repos)"
+        repo.git.commit("--allow-empty", m=message)
+        repo.git.push("-u", "origin", branch_name)
+
+        ref = repo.head.commit.hexsha
+
+        client = utils.Client(
+            test_env["api_url"], test_env["api_auth_type"], test_env.get("timeout")
+        )
+        payload = {
+            "repo": env_data["repo"],
+            "ref": ref,
+            "pkg_managers": [],
+            "flags": ["include-git-dir"],
+        }
+
+        initial_response = client.create_new_request(payload=payload)
+        completed_response = client.wait_for_complete_request(initial_response)
+
+        utils.assert_properly_completed_response(completed_response)
+        assert completed_response.data["packages"] == []
+        assert completed_response.data["dependencies"] == []
+
+        client.download_and_extract_archive(completed_response.id, tmp_path)
+        source_path = tmp_path / f"download_{str(completed_response.id)}"
+        downloaded_repo = Repo(source_path / "app")
+        assert downloaded_repo.head.commit.hexsha == ref
+        assert not downloaded_repo.git.diff()
+        assert not os.listdir(source_path / "deps")
+
+        utils.assert_content_manifest(client, completed_response.id, [])
+
+    finally:
+        repo.git.push("--delete", "origin", branch_name)
