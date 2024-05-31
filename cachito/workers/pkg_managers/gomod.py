containerbuildsystem/cachito

commit d9d1620ab789126b2232467f3e4802577719ab1c

pkg_managers: gomod: Enable Full Go 1.21 support
Commit 9f08f8e8 added preliminary support for Go 1.21 only, meaning
that we'd use a fixed toolchain version of 1.21.0 (or the bundled Go
toolchain, whichever was bigger). Some projects do bump even the micro
version of their required Go toolchain which means that we'd be
forced to refresh the container image with every Go micro release to
keep up with these bumps for the dependency prefetching (looking at you
Openshift).

Starting with Go 1.21, however, one is able to instruct Go to download
a newer toolchain automatically if the installed one doesn't comply
with the requirement in go.mod. That behaviour is controller by the
GOTOOLCHAIN variable which will need to be set as 'GOTOOLCHAIN=auto' to
override the default behaviour of Go (depending on the installation)
which always prefers the installed toolchain.

One thing to consider is the fact that a newer toolchain never
downloads an older one, meaning that if the project's build image
doesn't supply the desired version of Go (either with through the 'go'
line or the 'toolchain' line) and the consumer overrides the
GOTOOLCHAIN setting to 'auto', the build will fail in either case
because the correct toolchain version won't be available.
To mitigate this on our side we'll always base our processing on the
0th release of a minor Go version (i.e. 1.21.0, 1.22.0, etc.) which
ensures that with GOTOOLCHAIN=auto Go will always download and cache
any newer toolchain from any dependent module.

Note that we're only going to set GOTOOLCHAIN=auto for the dependency
fetching, i.e. in our own environment. Consumers are free
to override the GOTOOLCHAIN at their will in their build environment.
With that in mind, we're also dropping the GOTOOLCHAIN=local override
we have been forcing upon the users so far for the very same reason.

Inspired-by: https://github.com/containerbuildsystem/cachi2/commit/e0732c78f7df24de2b2a6af0750e40fa2b6396a1

cachito/workers/pkg_managers/gomod.py
@@ -312,7 +312,7 @@ def resolve_gomod(app_source_path, request, dep_replacements=None, git_dir_path=
             "GOPROXY": f"{athens_url}|{athens_url}",
             "PATH": os.environ.get("PATH", ""),
             "GOMODCACHE": "{}/pkg/mod".format(temp_dir),
-            "GOTOOLCHAIN": "local",
+            "GOTOOLCHAIN": "auto",
         }
         if "cgo-disable" in request.get("flags", []):
             env["CGO_ENABLED"] = "0"
@@ -1111,20 +1111,22 @@ def _select_go_toolchain(go_mod_file: Path) -> Go:
             f"Required/recommended Go toolchain version '{target_version}' is not supported yet.",
         )
 
-    if target_version >= go_max_version and go_base_version < go_max_version:
-        # our base Go installation is too old and we need a newer one to support new keywords
+    if target_version >= go_max_version:
+        # Project makes use of Go >=1.21:
+        # - always use the 'X.Y.0' toolchain to make sure GOTOOLCHAIN=auto fetches anything newer
+        # - container environments need to have it pre-installed
+        # - local environments will always install 1.21.0 SDK and then pull any newer toolchain
         go = Go(release="go1.21.0")
-    elif target_version < go_max_version and go_base_version >= go_max_version:
+    elif go_base_version >= go_max_version:
         # Starting with Go 1.21, Go doesn't try to be semantically backwards compatible in that
-        # the 'go X.Y' line now denotes the minimum required version of Go, no a "suggested"
-        # version. What it means in practice is that a Go toolchain >= 1.21 enforces the
-        # biggest common toolchain denominator across all dependencies and so if the input
-        # project specifies e.g. 'go 1.19' and **any** of its dependencies specify 'go 1.21'
-        # (or higher), then the default 1.21 toolchain will bump the input project's go.mod
-        # file to make sure the minimum required Go version is met across all dependencies.
-        # That is a problem, because it'll lead to fatal build failures forcing everyone to
-        # update their build recipes. Note that at some point they'll have to do that anyway,
-        # but until majority of projects in the ecosystem adopt 1.21, we need a fallback to an
-        # older toolchain version.
+        # the 'go X.Y' line now denotes the minimum required version of Go, not a "suggested"
+        # version. What it means in practice is that a Go toolchain >= 1.21 enforces the biggest
+        # common toolchain denominator across all dependencies and so if the input project
+        # specifies e.g. 'go 1.19' and **any** of its dependencies specify 'go 1.21' (or higher),
+        # then the default 1.21 toolchain will bump the input project's go.mod file to make sure
+        # the minimum required Go version is met across all dependencies. That is a problem,
+        # because it'll lead to fatal build failures forcing everyone to update their build
+        # recipes. Note that at some point they'll have to do that anyway, but until majority of
+        # projects in the ecosystem adopt 1.21, we need a fallback to an older toolchain version.
         go = Go(release="go1.20")
     return go

cachito/workers/tasks/gomod.py
@@ -166,7 +166,6 @@ def fetch_gomod_source(request_id, dep_replacements=None, package_configs=None):
         "GOCACHE": {"value": "deps/gomod", "kind": "path"},
         "GOPATH": {"value": "deps/gomod", "kind": "path"},
         "GOMODCACHE": {"value": "deps/gomod/pkg/mod", "kind": "path"},
-        "GOTOOLCHAIN": {"value": "local", "kind": "literal"},
     }
     env_vars.update(config.cachito_default_environment_variables.get("gomod", {}))
     update_request_env_vars(request_id, env_vars)

cachito/web/static/api_v1.yaml
@@ -541,9 +541,6 @@ paths:
                 GOSUMDB:
                   value: off
                   kind: literal
-                GOTOOLCHAIN:
-                  value: local
-                  kind: literal
         "404":
           description: The request wasn't found
           content:

cachito/workers/config.py
@@ -31,7 +31,6 @@ class Config(object):
     cachito_default_environment_variables = {
         "gomod": {
             "GOSUMDB": {"value": "off", "kind": "literal"},
-            "GOTOOLCHAIN": {"value": "local", "kind": "literal"},
         },
         "npm": {
             "CHROMEDRIVER_SKIP_DOWNLOAD": {"value": "true", "kind": "literal"},
@@ -164,7 +163,6 @@ class TestingConfig(DevelopmentConfig):
         "gomod": {
             "GO111MODULE": {"value": "on", "kind": "literal"},
             "GOSUMDB": {"value": "off", "kind": "literal"},
-            "GOTOOLCHAIN": {"value": "local", "kind": "literal"},
         },
         "npm": {
             "CHROMEDRIVER_SKIP_DOWNLOAD": {"value": "true", "kind": "literal"},

tests/test_workers/test_pkg_managers/test_gomod.py
@@ -1288,13 +1288,13 @@ def test_get_gomod_version_fail(go_mod_file: Path) -> None:
     "base_release, go_mod_file, expected_toolchain",
     [
         pytest.param("go1.20.7", "go 1.20", "1.20.7", id="old_base_version_equals_modfile"),
-        pytest.param("go1.21.4", "go 1.21.0", "1.21.4", id="new_base_version_equals_modfile"),
+        pytest.param("go1.21.4", "go 1.21.0", "1.21.0", id="new_base_version_equals_modfile"),
         pytest.param("go1.20.7", "go 1.21.0", "1.21.0", id="modfile_requires_newer_toolchain"),
-        pytest.param("go1.21.4", "go 1.21.9", "1.21.4", id="modfile_requires_newer_121_toolchain"),
+        pytest.param("go1.21.4", "go 1.21.9", "1.21.0", id="modfile_requires_newer_121_toolchain"),
         pytest.param("go1.21.4", "go 1.20", "1.20", id="modfile_requires_older_toolchain"),
         pytest.param("go1.20", "", "1.20", id="no_modfile_version_use_base_toolchain"),
         pytest.param("go1.21.4", "", "1.20", id="no_modfile_version_use_older_toolchain"),
-        pytest.param("go1.21.6", "toolchain go1.21.4", "1.21.6", id="decide_based_on_toolchain"),
+        pytest.param("go1.21.6", "toolchain go1.21.4", "1.21.0", id="decide_based_on_toolchain"),
     ],
     indirect=["go_mod_file"],
 )
@@ -1345,7 +1345,7 @@ class TestGo:
             pytest.param(
                 "/usr/bin/go1.21",
                 {
-                    "env": {"GOCACHE": "/foo", "GOTOOLCHAIN": "local"},
+                    "env": {"GOCACHE": "/foo", "GOTOOLCHAIN": "auto"},
                     "cwd": "/foo/bar",
                     "text": True,
                 },
@@ -1377,7 +1377,7 @@ class TestGo:
             pytest.param(
                 None,
                 {
-                    "env": {"GOCACHE": "/foo", "GOTOOLCHAIN": "local"},
+                    "env": {"GOCACHE": "/foo", "GOTOOLCHAIN": "auto"},
                     "cwd": "/foo/bar",
                     "text": True,
                 },
@@ -1473,7 +1473,7 @@ class TestGo:
         retry: bool,
     ) -> None:
 
-        env = {"env": {"GOTOOLCHAIN": "local", "GOCACHE": "foo", "GOPATH": "bar"}}
+        env = {"env": {"GOTOOLCHAIN": "auto", "GOCACHE": "foo", "GOPATH": "bar"}}
         opts = ["mod", "download"]
         go = gomod.Go(release=release)
         go(opts, retry=retry, params=env)

tests/test_workers/test_tasks/test_gomod.py
@@ -114,7 +114,6 @@ def test_fetch_gomod_source(
     env_vars = {
         "GO111MODULE": {"value": "on", "kind": "literal"},
         "GOSUMDB": {"value": "off", "kind": "literal"},
-        "GOTOOLCHAIN": {"value": "local", "kind": "literal"},
     }
     sample_env_vars.update(env_vars)
 

commit 5e63078ef9bb8753defe94b7748032565a979133

pkg_managers: gomod: Fix the Go toolchain selection conditions
When commit d9d1620a enabled full Go 1.21 support the semantics it used
for the Go toolchain selection conditions was very unfortunate as it
revolved too much around the 'go_max_version' variable which at the
time was set to be 1.21. The problem there is that once we try bumping
the max version to say 1.22 the conditions stop making sense and as a
bonus the existing tests start failing. The reasons for that are the
following:
  - we're not actually planning on updating the container image with
    each Go version we're going to support unless absolutely necessary;
    that is thanks to the GOTOOLCHAIN=auto mechanism introduced by 1.21
    that will download any new toolchain as needed automatically
  - without changing unit tests we suddenly can't pass the trivial
    condition on selecting the old fallback 1.20 if the existing unit
    test's base Go release is 1.21.X and the new max supported version
    is 1.22

Tweak the selection conditions so that they finally make sense
semantically:
- use go_max_version only to check incoming requests for incompatible
  new releases
- otherwise always explicitly compare against 1.21 and only fall back
  to 1.20 if the base release is above 1.21 which would lead to
  dirtying the source repo due to compatibility issues on Golang's side

Fixes: d9d1620ab789126b2232467f3e4802577719ab1c

Signed-off-by: Erik Skultety <eskultet@redhat.com>

cachito/workers/pkg_managers/gomod.py
@@ -1078,6 +1078,7 @@ def _select_go_toolchain(go_mod_file: Path) -> Go:
     go = Go()
     target_version = None
     go_max_version = pkgver.Version("1.21")
+    go_121_version = pkgver.Version("1.21")
     go_base_version = go.version
     go_mod_version_msg = "go.mod reported versions: '{}'[go], '{}'[toolchain]"
 
@@ -1111,13 +1112,13 @@ def _select_go_toolchain(go_mod_file: Path) -> Go:
             f"Required/recommended Go toolchain version '{target_version}' is not supported yet.",
         )
 
-    if target_version >= go_max_version:
+    if target_version >= go_121_version:
         # Project makes use of Go >=1.21:
         # - always use the 'X.Y.0' toolchain to make sure GOTOOLCHAIN=auto fetches anything newer
         # - container environments need to have it pre-installed
         # - local environments will always install 1.21.0 SDK and then pull any newer toolchain
         go = Go(release="go1.21.0")
-    elif go_base_version >= go_max_version:
+    elif go_base_version >= go_121_version:
         # Starting with Go 1.21, Go doesn't try to be semantically backwards compatible in that
         # the 'go X.Y' line now denotes the minimum required version of Go, not a "suggested"
         # version. What it means in practice is that a Go toolchain >= 1.21 enforces the biggest